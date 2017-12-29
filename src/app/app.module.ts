a999f9f46835df8f49d39e9e039840d1e74cca19
joniburn <joniburn@gmail.com>
2017-12-29 15:55:17 +0900

    ng generate component selectable-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SelectableTableComponent } from './selectable-table/selectable-table.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectableTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/selectable-table/selectable-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mst-selectable-table',
+  templateUrl: './selectable-table.component.html',
+  styleUrls: ['./selectable-table.component.scss']
+})
+export class SelectableTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
