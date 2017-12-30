cd7b7fa1a010ceb302e64d0e43e12ba66ea020c9
joniburn <joniburn@gmail.com>
2017-12-31 01:34:43 +0900

    見た目を調整してAppComponentをショーケースに改造

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { SelectableTableDataProvider } from './selectable-table';
 
 @Component({
   selector: 'mst-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'mst';
+  dataProvider = new SampleDataProvider();
+}
+
+class SampleDataProvider implements SelectableTableDataProvider {
+
+  /*
+   * 以下のようなテーブルを表示する。
+   * |    c1 |    c2 | ←キー名
+   * | Col 1 | Col 2 | ←表示名
+   * |-------|-------|
+   * |     1 |  text |
+   * |     2 |  text |
+   *   ...
+   * |   100 |  text |
+   */
+
+  headerDef: { [key: string]: string } = {
+    'c1': 'Col 1',
+    'c2': 'Col 2',
+  };
+
+  getRecords(pageIndex: number, pageSize: number): Observable<{ [key: string]: string }[]> {
+    const startRow = 1 + (pageIndex * pageSize);
+    const endRow = Math.min(startRow + pageSize, 101);
+    const data: { [key: string]: string }[] = [];
+
+    for (let row = startRow; row < endRow; row++) {
+      data.push({
+        'c1': `${row}`,
+        'c2': 'text',
+      });
+    }
+    return new BehaviorSubject(data);
+  }
+
 }
