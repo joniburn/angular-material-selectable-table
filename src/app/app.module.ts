338bfe42d560c3a305221803a126cec57165f1e6
joniburn <joniburn@gmail.com>
2017-12-31 18:01:38 +0900

    見た目とショーケースを調整し、ダークテーマに対応

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { SelectableTableModule } from './selectable-table';
     BrowserModule,
     BrowserAnimationsModule,
     SelectableTableModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
